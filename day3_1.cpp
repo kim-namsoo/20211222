c342853ad1ab7ba6ed1c87015e853da57a8e4ff6
coffeeman1229 <coffeeman1229@naver.com>
2021-12-22 13:52:31 +0900

    비주얼 스튜디오 삼각형 넓이 구하기

---
+++ day3_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float Height = 0.0f;
+	float Base = 0.0f;
+	float Area = 0.0f;
+
+	cin >> Height;
+	cin >> Base;
+
+	Area = (Height * Base) / 2.0f;
+	Area = (static_cast<float>(Height) * static_cast<float>(Base)) / 2.0f;
+
+	cout << Area;
+
+	return 0;
+}
